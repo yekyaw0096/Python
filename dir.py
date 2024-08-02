c69dbdd45aac8b88c436cc53d8795807b659cdc4
yekyaw0096 <yekyawoo.0096@gmail.com>
2024-08-02 16:11:54 +0700

    edit

--- dir.py
+++ dir.py
@@ -5,4 +5,4 @@ print("Dir ",dir(calculator))
 #for item in dir(calculator):
 #    print("Item ",item, " value ",eval(item))
     
-print ("Name ",__name__)
+print ("Name ",__name__) #check it is main method?
